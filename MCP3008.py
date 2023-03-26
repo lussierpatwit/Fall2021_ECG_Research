d2253581f9c1b26bba6071c2ba690ec6b572a2e5
lussierpatwit <60016954+lussierpatwit@users.noreply.github.com>
2023-03-26 12:00:11 -0400

    Update MCP3008.py

--- MCP3008.py
+++ MCP3008.py
@@ -1,5 +1,6 @@
 
 '''
+Written by Paul Lussier and Chen-Hsiang Yu (2021)
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
